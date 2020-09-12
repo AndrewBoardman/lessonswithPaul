c937e5a752609af0191042da0da9e915a3d102a3
Andrew Boardman <andrewcarlboardman@gmail.com>
2020-09-13 00:33:08 +0200

    Make code tidy

--- src/main/java/direct/booleandirect/examples/fizzbuzz/FizzBuzz.java
+++ src/main/java/direct/booleandirect/examples/fizzbuzz/FizzBuzz.java
@@ -14,40 +14,9 @@ public class FizzBuzz {
     }
 
     public String printFizzBuzz(int i) {
-
         return Optional.of(i)
                 .map(v -> (v % 3 == 0 ? "Fizz" : "") + (v % 5 == 0 ? "Buzz" : ""))
                 .map(v -> v.isEmpty() ? String.valueOf(i) : v)
                 .get();
-
-
-//        String output = (i % 3 == 0 ? "Fizz" : "") + (i % 5 == 0 ? "Buzz" : "");
-//        if (output.isEmpty()) {
-//            output = String.valueOf(i);
-//        }
-        // return output;
-
-//        String output = "";
-//        if (i % 3 == 0) {
-//            output += "Fizz";
-//        }
-//        if (i % 5 == 0) {
-//            output += "Buzz";
-//        }
-//        if (output.isEmpty()) {
-//            output = String.valueOf(i);
-//        }
-//        return output;
-
-
-//        if (i % 3 == 0 && i % 5 == 0) {
-//            return "FizzBuzz";
-//        } else if (i % 3 == 0) {
-//            return "Fizz";
-//        } else if (i % 5 == 0) {
-//            return "Buzz";
-//        } else {
-//            return String.valueOf(i);
-//        }
     }
 }
